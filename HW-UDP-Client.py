5ec29e2ebc7661f8dcce8f61aa6b1b410f88028c
jsl <s03851050@go.thu.edu.tw>
2023-05-30 13:22:09 +0800

    Update ports

--- HW-UDP-Client.py
+++ HW-UDP-Client.py
@@ -1,8 +1,8 @@
 from RDT_LIBRARY import RDTUtility
 
 # Setup Client
-server_addr = ('140.128.101.141', 8002)
-client_addr = ('172.23.8.60', 8001)
+server_addr = ('140.128.101.141', 7000)
+client_addr = ('172.23.8.60', 7000)
 client = RDTUtility(client_addr, server_addr)
 
 # run client
--- HW-UDP-Server.py
+++ HW-UDP-Server.py
@@ -1,8 +1,8 @@
 from RDT_LIBRARY import RDTUtility
 
 # Setup server
-server_addr = ('140.128.101.141', 8002)
-client_addr = ('172.23.8.60', 8001)
+server_addr = ('140.128.101.141', 7000)
+client_addr = ('172.23.8.60', 7000)
 server = RDTUtility(server_addr, client_addr)
 
 # run server
