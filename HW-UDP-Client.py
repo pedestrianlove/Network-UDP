a67bab5ea007346a0ad87a833573dc02e4da5c4e
jsl <s03851050@go.thu.edu.tw>
2023-05-30 12:51:55 +0800

    Test update

--- HW-UDP-Client.py
+++ HW-UDP-Client.py
@@ -1,103 +1,11 @@
+from RDT_LIBRARY import RDTUtility
 
-"""# 1. 設定環境"""
+# Setup Client
+server_addr = ('140.128.101.141', 8002)
+client_addr = ('172.23.8.60', 8001)
+client = RDTUtility(client_addr, server_addr)
 
-# Import dependencies for TCP/UDP
-import socket
-import time
-HOST = '140.128.101.141'
-PORT = 7000
+# run client
+client.run_client()
 
-# Setup Client IP and port
-server_addr = (HOST, PORT)
-
-# Setup renderImage() function
-import cv2
-from matplotlib import pyplot as plt
-def renderImage(imagePath):
-  imageObject = cv2.imread(imagePath, -1)
-  plt.imshow(imageObject)
-  plt.axis("off")
-  plt.show()
-
-import pickle
-
-# Setup packet forging function
-def packet(seq, binary_data):
-  # combine the sequence number and payload data into a tuple
-  packet = (seq, binary_data)
-  # serialize the packet to bytes using pickle
-  packet_bytes = pickle.dumps(packet)
-
-  return packet_bytes
-
-# Setup packet decode function
-def dec_packet(binary_packet):
-  # deserialize the packet from bytes using pickle
-  packet = pickle.loads(binary_packet)
-  seq, binary_data = packet
-  return seq, binary_data
-  
-# Setup function for udp listening
-def UDP_LISTEN(SOCKET_ADDR):
-  # Setup socket for recieving ACK packet
-  conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  # Requesting client port to listen on
-  conn.bind(SOCKET_ADDR)
-  return conn
-
-# Setup function for udp sending
-def UDP_PREPARE():
-  # Setup socket for recieving ACK packet
-  conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  return conn
-
-"""## c. 設定客戶端"""
-
-def client():
-  # Setup target socket
-  s = UDP_PREPARE()
-  s.connect(server_addr)
-  #client_addr = s.getsockname()
-  client_addr = ('172.23.8.60', 6000)
-  # Setup socket for recieving ACK packet
-  r = UDP_LISTEN(client_addr)
-
-  # Client starting
-  print("Client: Client starting at...", str(client_addr))
-
-  # Sending file
-  print("Client: Sending the following image:")
-  renderImage("test.jpg")
-  counter = 0
-  testFile = open('test.jpg', 'rb')
-  buf = testFile.read(1024)
-  while (buf):
-    print("Client: Sending packet with SEQ...", counter)
-    s.sendto(packet(counter, buf), server_addr)
-    seq, data_binary = dec_packet(r.recvfrom(1024 * 2)[0])
-    while (seq != counter or data_binary.decode() != "ACK"):
-      print("Client: Not receiving packet with ACK...", counter, ", resending...")
-      s.sendto(packet(counter, buf), server_addr)
-      seq, data_binary = dec_packet(r.recvfrom(1024 * 2)[0])
-    print ("Client: Received packet with ACK...", counter)
-    counter += 1
-    buf = testFile.read(1024)
-  
-  # Stopping client
-  print("File sent.")
-  s.sendto(packet(counter, "stop".encode()), server_addr)
-  seq, data_binary = dec_packet(r.recvfrom(1024 * 2)[0])
-  while(seq != counter or data_binary.decode() != "ACK"):
-    print("Client: Not receiving packet with ACK...", counter, ", resending...")
-    s.sendto(packet(counter, buf), server_addr)
-    seq, data_binary = dec_packet(r.recvfrom(1024 * 2)[0])
-
-  s.close()
-  print("Client stopped.")
-
-"""# 2. 執行"""
-
-client()
 
---
+++ RDT_LIBRARY.py
@@ -0,0 +1,102 @@
+import socket
+import pickle
+
+class RDTUtility:
+    def __init__(self, server_addr, client_addr):
+        self.server_addr = server_addr
+        self.client_addr = client_addr
+        self.server_socket = self.create_socket()
+        self.client_socket = self.create_socket()
+        self.sequence_number = 0
+
+    def create_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return sock
+
+    def packet(self, seq, binary_data):
+        packet = (seq, binary_data)
+        packet_bytes = pickle.dumps(packet)
+        return packet_bytes
+
+    def dec_packet(self, binary_packet):
+        packet = pickle.loads(binary_packet)
+        seq, binary_data = packet
+        return seq, binary_data
+
+    def send_packet(self, socket, packet):
+        socket.sendto(packet, self.server_addr)
+
+    def receive_packet(self, socket):
+        return socket.recvfrom(1024 * 2)[0]
+
+    def send_ack(self, socket, seq):
+        ack_packet = self.packet(seq, "ACK".encode())
+        self.send_packet(socket, ack_packet)
+
+    def is_expected_seq(self, seq):
+        return seq == self.sequence_number
+
+    def rdt_send(self, socket, data):
+        packet = self.packet(self.sequence_number, data)
+        while True:
+            self.send_packet(socket, packet)
+            ack_packet = self.receive_packet(socket)
+            seq, _ = self.dec_packet(ack_packet)
+            if self.is_expected_seq(seq):
+                self.sequence_number += 1
+                break
+
+    def rdt_receive(self, socket):
+        while True:
+            packet = self.receive_packet(socket)
+            seq, data = self.dec_packet(packet)
+            if self.is_expected_seq(seq):
+                self.send_ack(socket, seq)
+                self.sequence_number += 1
+                return data
+
+    def start_server(self):
+        print("Server: Server starting...")
+        self.server_socket.bind(self.server_addr)
+        print("Server: Listening at: ", str(self.server_addr))
+        print("Server: Waiting for data...")
+
+        outputFile = open('received.jpg', 'wb')
+        while True:
+            data = self.rdt_receive(self.server_socket)
+            if data.decode() == "stop":
+                break
+            outputFile.write(data)
+        print("Server: File closed.")
+        outputFile.close()
+
+        print("Server: Server stopped.")
+        self.server_socket.close()
+
+    def start_client(self):
+        print("Client: Client starting at...", str(self.client_addr))
+
+        print("Client: Sending the following image:")
+        renderImage("test.jpg")
+
+        counter = 0
+        testFile = open('test.jpg', 'rb')
+        buf = testFile.read(1024)
+        while buf:
+            print("Client: Sending packet with SEQ...", counter)
+            self.rdt_send(self.client_socket, buf)
+            counter += 1
+            buf = testFile.read(1024)
+
+        print("Client: Sending stop signal...")
+        self.rdt_send(self.client_socket, "stop".encode())
+
+        testFile.close()
+        print("Client stopped.")
+
+    def renderImage(self, imagePath):
+        imageObject = cv2.imread(imagePath, -1)
+        plt.imshow(imageObject)
+        plt.axis("off")
+        plt.show()
