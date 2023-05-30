34994adffdf38ab528e2d98833d066d18d441555
JHIH-SIOU LI <j.s.li@gapp.nthu.edu.tw>
2023-05-30 14:28:52 +0800

    Update

--- HW-UDP-Client.py
+++ HW-UDP-Client.py
@@ -3,7 +3,7 @@ from RDT_LIBRARY import RDTUtility
 # Setup Client
 server_addr = ('140.128.101.141', 7000)
 client_addr = ('172.23.8.60', 7000)
-client = RDTUtility(client_addr, server_addr)
+client = RDTUtility(server_addr, client_addr)
 
 # run client
 client.start_client()
