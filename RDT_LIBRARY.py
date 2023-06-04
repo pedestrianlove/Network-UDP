080183650eefc336903e1282ea436ef64580521a
jsl <s03851050@go.thu.edu.tw>
2023-06-04 13:44:17 +0800

    Refactored the Packet class into a different file. (separation of concerns)

--- RDT_LIBRARY.py
+++ RDT_LIBRARY.py
@@ -1,39 +1,6 @@
 import socket
-import pickle
 
-
-class Packet:
-    seq = None
-    binary_data = None
-
-    def __init__(self, seq_input, data_input):
-        self.seq = seq_input
-        self.binary_data = data_input
-
-    def __str__(self):
-        return "Packet: SEQ=" + str(self.seq) + ", DATA=" + str(self.binary_data)
-
-    # Utility Methods
-    def encode(self):
-        packet = (self.seq, self.binary_data)
-        packet_bytes = pickle.dumps(packet)
-        return packet_bytes
-
-    @staticmethod
-    def decode(binary_packet):
-        packet = pickle.loads(binary_packet)
-        seq, binary_data = packet
-        return Packet(seq, binary_data)
-
-    # Userspace methods
-    def send(self, assigned_socket, addr):
-        packet = self.encode()
-        assigned_socket.sendto(packet, addr)
-
-    @staticmethod
-    def receive(assigned_socket):
-        binary_packet = assigned_socket.recvfrom(1024 * 2)[0]
-        return Packet.decode(binary_packet)
+from Packet import Packet
 
 
 class RDTUtility:
