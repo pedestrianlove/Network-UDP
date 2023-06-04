b07a9074bebe426cf3c4549e0a4da8c04cea7f4b
jsl <s03851050@go.thu.edu.tw>
2023-06-04 15:36:01 +0800

    GBN.

--- RDT_LIBRARY.py
+++ RDT_LIBRARY.py
@@ -1,15 +1,22 @@
+import asyncio
 import socket
 
 from Packet import Packet
 
 
 class RDTUtility:
+    # General variables
     timeout = None
     server_addr = None
     server_socket = None
     client_addr = None
     sequence_number = None
     client_socket = None
+
+    # GBN variables
+    window_size = None
+    base_ptr = None
+    failed = None
 
     @classmethod
     def __init__(cls, server_addr, client_addr):
@@ -19,6 +26,7 @@ class RDTUtility:
         cls.client_socket = cls.create_socket()
         cls.sequence_number = 0
         cls.timeout = 5  # Set timeout value in seconds
+        cls.window_size = 100
 
     @staticmethod
     def create_socket():
@@ -35,27 +43,51 @@ class RDTUtility:
     def is_expected_seq(cls, seq):
         return seq == cls.sequence_number
 
-    # Userspace methods
-    @classmethod
-    def rdt_send(cls, data):
-        packet = Packet(cls.sequence_number, data)
-        while True:
-            print("RDT: Sending the packet with SEQ=", packet.seq, "...")
-            packet.send(cls.client_socket, cls.server_addr)
+    async def receive_ack(self):
+        checked_ptr = self.base_ptr
+        while checked_ptr < self.base_ptr + self.window_size:
             try:
-                cls.client_socket.settimeout(cls.timeout)
-                ack_packet = Packet.receive(cls.client_socket)
-                if cls.is_expected_seq(ack_packet.seq) and ack_packet.binary_data.decode() == "ACK":
-                    print("RDT: Correct ACK SEQ received, goes on sending another one...")
-                    cls.sequence_number += 1
-                    break
+                self.client_socket.settimeout(self.timeout)
+                ack_packet = Packet.receive(self.client_socket)
+                if self.is_expected_seq(ack_packet.seq) and ack_packet.binary_data.decode() == "ACK":
+                    print("RDT: Correct ACK SEQ received, shifting the window...")
+                    self.sequence_number += 1
+                    checked_ptr += 1
                 else:
                     print("RDT: Incorrect ACK SEQ received,", ack_packet.seq, ", sending another one...")
                     print("RDT: ", ack_packet)
-                    continue
+
+                # Set flags to resend the window
+                self.failed = True
             except TimeoutError:
-                print("RDT: Timeout occurred, resending the packet...")
+                self.failed = True
                 continue
+        return True
+
+    # Userspace methods
+    def rdt_send(self, packets_list):
+        list_length = len(packets_list)
+        self.base_ptr = 0
+        self.failed = True
+
+        # Start listening for ACK packets
+        event_loop = asyncio.get_event_loop()
+        ack_result = event_loop.create_task(self.receive_ack())
+
+        while self.base_ptr < list_length:
+            # Send packets on request
+            if self.failed:
+                self.failed = False
+                # Get current window
+                local_base_ptr = self.base_ptr
+                print("RDT: Sending the packets with SEQ=", local_base_ptr, ", to ", local_base_ptr + self.window_size, "...")
+                for i in range(local_base_ptr, local_base_ptr + self.window_size):
+                    if i >= list_length:
+                        break
+                    packets_list[i].send(self.client_socket, self.client_addr)
+
+        # Pickup the result of the ACK packets listening
+        event_loop.run_until_complete(ack_result)
 
     @classmethod
     def rdt_receive(cls):
@@ -106,12 +138,19 @@ class RDTUtility:
         cls.client_socket.bind(cls.client_addr)
         print("Client: Sending the following image: test.jpg")
 
+        # Read the binary data from file and buffer into packets list
+        packets_list = []
+        counter = 0
         testFile = open('test.jpg', 'rb')
         buf = testFile.read(1024)
         while buf:
-            print("Client: Sending packet with SEQ...", cls.sequence_number)
-            cls.rdt_send(buf)
+            print("Client: Buffering packet with SEQ...", counter)
+            packets_list.append(Packet(counter, buf))
+            counter += 1
             buf = testFile.read(1024)
+
+        # Send the buffered packets
+        cls.rdt_send(packets_list)
 
         print("Client: File sent.")
         print("Client: Sending stop signal...")
